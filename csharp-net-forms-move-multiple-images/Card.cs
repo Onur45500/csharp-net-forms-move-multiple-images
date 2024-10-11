8568332def2a2d5a8714a34c13a348aaae2ae02d
Onur45500 <onur.akmese@viacesi.fr>
2024-10-11 15:00:15 +0200

    Added a card class

---
+++ csharp-net-forms-move-multiple-images/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_net_forms_move_multiple_images
+{
+    public class Card
+    {
+        public Image cardPic;
+        public int width;
+        public int height;
+        public Point position = new Point();
+        public bool active = false;
+        public Rectangle rect;
+
+        public Card(string imageLocation)
+        {
+            cardPic = Image.FromFile(imageLocation);
+            width = cardPic.Width;
+            height = cardPic.Height;
+            rect = new Rectangle(position.X, position.Y, width, height);
+        }
+    }
+}
