ed15c1b6decc5be2bdaea1b68fc3d3cc71a3f6ea
Onur45500 <onur.akmese@viacesi.fr>
2024-10-11 14:26:10 +0200

    Initialized the project

---
+++ csharp-net-forms-move-multiple-images/Form1.cs
@@ -0,0 +1,10 @@
+namespace csharp_net_forms_move_multiple_images
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
